bc3e2bc87a4ff6c02e9eed779b6de14a1b956a1a
Sushma Tripathi <sushma.tripathi@hcl.com>
2024-07-01 15:15:01 +0530

    SA update

--- OneDrive - HCL Technologies Ltd/Desktop/GITHUB/daslearning/ep1/main.tf
+++ OneDrive - HCL Technologies Ltd/Desktop/GITHUB/daslearning/ep1/main.tf
@@ -1,7 +1,7 @@
 # Google reources
 
 resource "google_storage_bucket" "bucket" {
-  project  = "dl-k8s-dev1cade"
+  project  = "ayush-424308"
   name     = "daslearning-bucket-23"
   location = "ASIA-SOUTH1"
 }
